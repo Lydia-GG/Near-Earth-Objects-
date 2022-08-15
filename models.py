9ef1a08ced4d129281cc283d595d1b1a809f839a
Lydia Samir <L-Samir@hotmail.com>
2022-08-15 17:01:55 +0200

    finished task4

--- models.py
+++ models.py
@@ -32,14 +32,12 @@ class NearEarthObject:
     initialized to an empty collection, but eventually populated in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
     def __init__(self, designation, name=None, diameter=float('nan'), hazardous=""):
         """Create a new `NearEarthObject`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
         """
-        # TODO: Assign information from the arguments passed to the constructor
         # onto attributes named `designation`, `name`, `diameter`, and `hazardous`.
         # You should coerce these values to their appropriate data type and
         # handle any edge cases, such as a empty name being represented by `None`
@@ -47,7 +45,6 @@ class NearEarthObject:
         self.designation = designation
         self.name = None if name == "" else name
         self.diameter = float('nan') if diameter == "" else float(diameter)
-        # self.hazardous = False if hazardous == "" or hazardous == "N" else hazardous
         self.hazardous = True if hazardous == 'Y' else False
 
         # Create an empty initial collection of linked approaches.
@@ -56,12 +53,12 @@ class NearEarthObject:
     @property
     def fullname(self):
         """Return a representation of the full name of this NEO."""
-        # TODO: Use self.designation and self.name to build a fullname for this object.
+
         return f'{self.designation} {self.name}'
 
     def __str__(self):
         """Return `str(self)`."""
-        # TODO: Use this object's attributes to return a human-readable string representation.
+
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
         self.hazardous = f"is" if self.hazardous else "is not"
@@ -86,19 +83,19 @@ class CloseApproach:
     private attribute, but the referenced NEO is eventually replaced in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
+
     # If you make changes, be sure to update the comments in this file.
     def __init__(self, designation, time, distance, velocity):
         """Create a new `CloseApproach`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
         """
-        # TODO: Assign information from the arguments passed to the constructor
+
         # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
         # You should coerce these values to their appropriate data type and handle any edge cases.
         # The `cd_to_datetime` function will be useful.
         self._designation = designation
-        self.time = cd_to_datetime(time)  # TODO: Use the cd_to_datetime function for this attribute.
+        self.time = cd_to_datetime(time)  
         self.distance = float(distance)
         try:
           self.velocity = float(velocity)
@@ -122,14 +119,12 @@ class CloseApproach:
         formatted string that can be used in human-readable representations and
         in serialization to CSV and JSON files.
         """
-        # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
+
         # build a formatted representation of the approach time.
-        # TODO: Use self.designation and self.name to build a fullname for this object.
         return datetime_to_str(self.time)
 
     def __str__(self):
         """Return `str(self)`."""
-        # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
         return f"On {self.time}, {self._designation} approaches Earth at a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s."
--- write.py
+++ write.py
@@ -13,6 +13,10 @@ You'll edit this file in Part 4.
 import csv
 import json
 
+from helpers import datetime_to_str
+
+
+
 
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
@@ -25,7 +29,17 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    with open(filename, "w") as outfile:
+      writer = csv.writer(outfile)
+      writer.writerow(fieldnames)
+      for result in results:
+        result.neo.name = result.neo.name if result.neo.name is not None else ""
+        result.neo.diameter = result.neo.diameter if result.neo.diameter is not None else ""
+
+        data = [result.time,result.distance, result.velocity, result._designation, result.neo.name, result.neo.diameter, result.neo.hazardous]
+
+        writer.writerow(data)
 
 
 def write_to_json(results, filename):
@@ -39,4 +53,27 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+
+    with open(filename, "w") as outfile:
+      data = []
+      for result in results:
+        result.neo.name = result.neo.name if result.neo.name is not None else ""
+        result.neo.diameter = result.neo.diameter if result.neo.diameter is not None else float("nan")
+        result.neo.hazardous = bool(True) if result.neo.hazardous else bool(False)
+        
+        result_data ={
+                        "datetime_utc": datetime_to_str(result.time),
+                        "distance_au": float(result.distance),
+                        "velocity_km_s": float(result.velocity),
+                        "neo": {
+                          "designation": str(result.neo.designation),
+                          "name": result.neo.name,
+                          "diameter_km": result.neo.diameter,
+                          "potentially_hazardous": result.neo.hazardous
+                        }
+                      }
+
+        data.append(result_data)
+      json.dump(data, outfile, indent=2)
+
+
